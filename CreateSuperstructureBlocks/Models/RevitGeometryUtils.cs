ca99c17d38461fe450015be56523d4107ee33c33
Денис Щербатюк <denvol100@gmail.com>
2023-11-02 15:12:20 +0300

    Fix bug when plane intersect roadline more then one time

--- CreateSuperstructureBlocks/Models/RevitGeometryUtils.cs
+++ CreateSuperstructureBlocks/Models/RevitGeometryUtils.cs
@@ -122,6 +122,8 @@ namespace CreateSuperstructureBlocks.Models
         // Получение линии из списка, которая пересекается с плоскостью
         public static Line GetIntersectCurve(IEnumerable<Line> lines, Plane plane)
         {
+            var intersectionLines = new List<Line>();
+
             XYZ originPlane = plane.Origin;
             XYZ directionLine = plane.XVec;
 
@@ -141,8 +143,17 @@ namespace CreateSuperstructureBlocks.Models
                 var compResult = lineByPlane.Intersect(baseLine, out result);
                 if (compResult == SetComparisonResult.Overlap)
                 {
-                    return line;
+                    intersectionLines.Add(line);
                 }
+            }
+
+            if (intersectionLines.Count == 1)
+            {
+                return intersectionLines.First();
+            }
+            else if (intersectionLines.Count > 1)
+            {
+                return intersectionLines.OrderBy(l => l.Evaluate(0.5, true).DistanceTo(plane.Origin)).First();
             }
 
             return null;
